5d820074b79f4751b6787fcc88bb05fd4abc2465
AkielCh <akiel98@hotmail.com>
2023-08-12 10:58:46 +0100

    Fundamental part 1 complete

---
+++ 01-Fundamentals-Part-1/starter/index.js
@@ -0,0 +1,12 @@
+//---------
+//Conditional operator
+/*
+// if bil is 50 - 300 then tip 15
+// else tip 20
+const bill = 275;
+
+let tip;
+// let tip = bill>=50 && bill<=300? bill*0.15:bill*0.2
+console.log(`The bill was ${bill}, the tip was ${ tip = bill>=50 && bill<=300? bill*0.15:bill*0.2}, and the total value ${bill+tip}`)
+*/
+//----------
